d68686655cc8c495edd9f5a4d94fd8949feaf8a3
astrapi69 <asterios.raptis@gmx.net>
2021-10-22 11:42:42 +0200

    4 files modified

--- src/app/counter.actions.ts
+++ src/app/counter.actions.ts
@@ -1,5 +1,5 @@
 import { createAction } from '@ngrx/store';
 
-export const increment = createAction('[Counter Component] Increment');
+export const incrementAction = createAction('[Counter Component] Increment');
 export const decrement = createAction('[Counter Component] Decrement');
 export const reset = createAction('[Counter Component] Reset');
